e1c71e9620d3dd356a3b531355f95c6b0f6dc8ea
lisiks <mihailsirokov1956@gmail.com>
2026-09-25 18:09:39 +0300

    Добавляем PostgreSQL в проект

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import computed_field
 
 class AppSettings(BaseSettings):
     port: int
@@ -6,6 +7,23 @@ class AppSettings(BaseSettings):
     model_config = SettingsConfigDict(
         case_sensitive=False,
         env_prefix="APP_",
+        extra="ignore",
+    )
+
+class DBSettings(BaseSettings):
+    host: str
+    port: int
+    user: str
+    password: str
+    name: str
+
+    @computed_field
+    def url(self) -> str:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
+
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        env_prefix="DB_",
         extra="ignore",
     )
 
